d47e159de9e00900dc64bc2d2333d5b23971f6ed
r48cheng <r48cheng@uwaterloo.ca>
2023-04-14 18:08:36 -0400

    add all basic models and database migrations

--- app/models.py
+++ app/models.py
@@ -11,10 +11,10 @@ class Employees(Base):
     id = Column(Integer, primary_key=True, nullable=False)
     # here we assume all employees is default to be role_id 0 (means "intern" in our roles table, minimum authorization in our clinic)
     # fk should be added later, since we do not have these two tables for now
-    role_id = Column(Integer, ForeignKey("roles.rid", ondelete="SET DEFAULT"), server_default="0", nullable=False)
+    role_id = Column(Integer, ForeignKey("roles.rid", ondelete="CASCADE"), server_default="0", nullable=False)
     # when we delete a contract, we delete this employee
     contract_id = Column(Integer, ForeignKey("contracts.cid", ondelete="CASCADE"), nullable=False)
-    
+
     full_name = Column(String, nullable=False)
     email = Column(String, nullable=False)
     tel = Column(String, nullable=False)
@@ -40,3 +40,26 @@ class Contracts(Base):
     contract_duration = Column(Integer, nullable=False, server_default="1")
     salary = Column(Integer, nullable=False)
 
+class Patients(Base):
+    __tablename__ = 'patients'
+    pid = Column(Integer, primary_key=True, nullable=False)
+    full_name = Column(String, nullable=False)
+    email = Column(String, nullable=False)
+    gender = Column(Boolean, nullable=False)  # 0 means female, 1 means male
+    tel = Column(String, nullable=False)
+    address = Column(String, nullable=False)
+    insurance_number = Column(String, nullable=False)
+    surgery_history = Column(String, nullable=False)
+    allergy = Column(String, nullable=False)
+    drug_history = Column(String, nullable=False)
+    modified_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text('now()'))
+
+class Appointments(Base):
+    __tablename__ = 'appointments'
+    aid = Column(Integer, primary_key=True, nullable=False)
+    patient_id = Column(Integer, ForeignKey("patients.pid", ondelete="CASCADE"), nullable=False)
+    doctor_id = Column(Integer, ForeignKey("employees.id", ondelete="CASCADE"), nullable=False)
+    notes = Column(String, nullable=False)
+    start_time = Column(TIMESTAMP(timezone=True), nullable=False)
+    end_time = Column(TIMESTAMP(timezone=True), nullable=False)
+    modified_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text('now()'))
